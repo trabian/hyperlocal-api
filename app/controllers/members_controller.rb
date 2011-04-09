cce5b3f2e7ba376799011b662c4e69c7e6bd7a42
Matt Dean <matt@trabian.com>
2011-04-09 10:44:49 -0400

    Cleaned up members.json API output

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,5 +5,33 @@ class MembersController < ApplicationController
   actions :index, :show, :update
 
   respond_to :json
+  
+  def index
+    super do |format|
+      format.json do
+        render :json => collection.to_json(json_api_options)
+      end
+    end
+  end
+
+  def show
+    super do |format|
+      format.json do
+        render :json => resource.to_json(json_api_options)
+      end
+    end
+  end
+
+protected
+
+  def json_api_options
+    {
+      :only => [
+        :id,
+        :first_name,
+        :last_name
+      ]
+    }
+  end
 
 end
